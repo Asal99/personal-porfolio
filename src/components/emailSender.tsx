ed6c0c8e55e1dd8591bbb0bfa0fc547904f6bba3
Asal99 <asalpoudel9@gmail.com>
2026-06-30 17:59:28 +0545

    Update EmailJS package

--- src/components/emailSender.tsx
+++ src/components/emailSender.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 type Props = {
   formData: {
@@ -20,7 +20,7 @@ const EmailSender: React.FC<Props> = ({ formData, onSuccess, onError }) => {
         "service_dg7uab8",
         "template_54aq9kb",
         formData,
-        "s_1ounUH92kIaR3Gt"
+        "s_1ounUH92kIaR3Gt",
       );
       onSuccess();
     } catch (error) {
